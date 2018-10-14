9627d7d234be407fec8f76cd78a80ad2014dd6a5
Davidj231996 <alerodiez8dj@hotmail.com>
2018-10-15 00:10:50 +0200

    Test Pruebas

--- src/test_pruebas.py
+++ src/test_pruebas.py
@@ -4,9 +4,9 @@ from pruebas import Noticiario
 def test_crear_Noticia():
 	n = Noticiario()
 	assert n.crear_Noticia("Golf","El golf es un deporte")==False, "Error en la creación de la noticia"
-	assert n.crear_Noticia("El golf","El golf es un deporte en crecimiento") ==True
+	assert n.crear_Noticia("El golf y su crecimiento","El golf es un deporte en crecimiento") ==True
 
 def test_crear_Usuario():
 	n = Noticiario()
-	assert n.crear_Usuario("A","B") ==0, "Error en la creación de un usuario"
-	assert n.crear_Usuario("David","Rubiales") ==0, "Error en la creacion de un usuario"
+	assert n.crear_Usuario("A","B") ==False, "Error en la creación de un usuario"
+	assert n.crear_Usuario("David","Rubiales") ==True
