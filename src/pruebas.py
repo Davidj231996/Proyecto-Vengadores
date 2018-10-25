0d3d244b3d1bc0cd50cf66be55aee2b24719d610
Davidj231996 <alerodiez8dj@hotmail.com>
2018-10-25 13:58:43 +0200

    Actualizando

--- src/pruebas.py
+++ src/pruebas.py
@@ -5,44 +5,44 @@ class Noticiario():
 	"""Clase para el microservicio Noticiario"""
 
 	def __init__(self):
-	noticia_i = 0
-	try:
-		if os.path.exists('data/noticias.json'):
-			with open('data/noticias.json', 'r') as f:
-			self.noticias = json.load(f)
-		else:
-			raise IOError("No se encuentra 'noticias.json'")
+		noticia_i = 0
+		try:
+			if os.path.exists('data/noticias.json'):
+				with open('data/noticias.json', 'r') as f:
+				self.noticias = json.load(f)
+			else:
+				raise IOError("No se encuentra 'noticias.json'")
 
-		if os.path.exists('data/categorias.json'):
-			with open('data/categorias.json', 'r'r) as f:
-			self.categorias = json.load(f)
-		else:
-			raise IOError("No se encuentra 'data/categorias.json'")
+			if os.path.exists('data/categorias.json'):
+				with open('data/categorias.json', 'r'r) as f:
+				self.categorias = json.load(f)
+			else:
+				raise IOError("No se encuentra 'data/categorias.json'")
 
-	except IOError as fallo:
-		print("Error {} leyendo hitos.json".format( fallo ) )
+		except IOError as fallo:
+			print("Error {} leyendo hitos.json".format( fallo ) )
 
-	def cuentaNoticias(self):
-		self.noticia_i = len(self.noticias)
-		return len(self.noticias)
+		def cuentaNoticias(self):
+			self.noticia_i = len(self.noticias)
+			return len(self.noticias)
 
-	def crear_Noticia(self,titulo,descripcion,categoria):
-		if(len(titulo) < 10):
+		def crear_Noticia(self,titulo,descripcion,categoria):
+			if(len(titulo) < 10):
+				return False
+			elif(len(titulo) > 50):
+				return False
+			elif(len(descripcion) < 0):
+				return False
+			elif(!categoria_correcta(categoria)):
+				return False
+			self.noticias[noticia_i]["titulo"] = titulo
+			self.noticias[noticia_i]["descripcion"] = descripcion
+			self.noticias[noticia_i]["categoria"] = categoria
+
+			return True
+
+		def categoria_correcta(self,categoria):
+			for i in self.categorias:
+				if(i == categoria):
+					return True
 			return False
-		elif(len(titulo) > 50):
-			return False
-		elif(len(descripcion) < 0):
-			return False
-		elif(!categoria_correcta(categoria)):
-			return False
-		self.noticias[noticia_i]["titulo"] = titulo
-		self.noticias[noticia_i]["descripcion"] = descripcion
-		self.noticias[noticia_i]["categoria"] = categoria
-
-		return True
-
-	def categoria_correcta(self,categoria):
-		for i in self.categorias:
-			if(i == categoria):
-				return True
-		return False
