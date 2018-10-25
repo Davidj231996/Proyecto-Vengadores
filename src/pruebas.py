e34767a259ad8d27d32ba1e75c9c06507bd5f4e1
Davidj231996 <alerodiez8dj@hotmail.com>
2018-10-25 13:46:48 +0200

    Closes #11

--- src/pruebas.py
+++ src/pruebas.py
@@ -1,22 +1,48 @@
+import json
+import os
+
 class Noticiario():
 	"""Clase para el microservicio Noticiario"""
 
-	def crear_Noticia(self,titulo,descripcion):
+	def __init__(self):
+	self.noticia_i = 0
+        try:
+            if os.path.exists('data/noticias.json'):
+                with open('data/noticias.json', 'r') as f:
+                    self.noticias = json.load(f)
+            else:
+                raise IOError("No se encuentra 'noticias.json'")
+
+	    if os.path.exists('data/categorias.json'):
+		with open('data/categorias.json', 'r'r) as f:
+		    self.categorias = json.load(f)
+	    else:
+		raise IOError("No se encuentra 'data/categorias.json'")
+
+        except IOError as fallo:
+             print("Error {} leyendo hitos.json".format( fallo ) )
+
+	def cuentaNoticias(self):
+		self.noticia_i = len(self.noticias)
+		return len(self.noticias)
+
+	def crear_Noticia(self,titulo,descripcion,categoria):
 		if(len(titulo) < 10):
 			return False
 		elif(len(titulo) > 50):
 			return False
 		elif(len(descripcion) < 0):
 			return False
-		else:
-			return True
+		elif(!categoria_correcta(categoria)):
+			return False
+		self.noticias[noticia_i]["titulo"] = titulo
+		self.noticias[noticia_i]["descripcion"] = descripcion
+		self.noticias[noticia_i]["categoria"] = categoria
 
-	def crear_Usuario(self,nombre,alias):
-		if(len(nombre) < 3):
-			return False
-		elif(len(alias) < 5):
-			return False
-		elif(len(alias) > 15):
-			return False
-		else:
-			return True
+		return True
+
+	def categoria_correcta(self,categoria):
+		for i in self.categorias:
+			if(i == categoria):
+				return True
+		return False
--- src/test_pruebas.py
+++ src/test_pruebas.py
@@ -1,12 +1,14 @@
 import pytest
 from pruebas import Noticiario
 
+def test_cuentaNoticias():
+	n = Noticiario()
+	assert n.cuentaNoticias() == 2
+
 def test_crear_Noticia():
 	n = Noticiario()
-	assert n.crear_Noticia("Golf","El golf es un deporte")==False, "Error en la creación de la noticia"
-	assert n.crear_Noticia("El golf y su crecimiento","El golf es un deporte en crecimiento") ==True
+	assert n.crear_Noticia("El Clásico más importante","El Clásico del 28-O es de vital importancia para ambos equipos","Futbol")==True
 
-def test_crear_Usuario():
+def test_cuentaNoticias_postCreacion():
 	n = Noticiario()
-	assert n.crear_Usuario("A","B") ==False, "Error en la creación de un usuario"
-	assert n.crear_Usuario("David","Rubiales") ==True
+	assert n.cuentaNoticias() == 3
