84da8f78cb552a98fefc06374cb5aa954d76ecb4
Davidj231996 <alerodiez8dj@hotmail.com>
2018-11-13 00:17:59 +0100

    Closes #19

--- API_WEB.py
+++ API_WEB.py
@@ -98,4 +98,4 @@ def page_not_found(error):
   		return "Página no encontrada", 404
 
 if __name__ == "__main__":
-    app.run(host='https://proyecto-vengadores.com', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
